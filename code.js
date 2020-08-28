479468d81a4580c116dab4d9b47cdd561e54e895
Edinyanga O. Ottoho <elzucky@gmail.com>
2020-08-28 01:38:54 +0100

    Update code.js

--- code.js
+++ code.js
@@ -1,16 +1,5 @@
 const readline = require('readline');
 
-/**
-    Instructions 
-    - Upon completion the program should let two players play Tic Tac Toe 
-    - You can implement your own functions when you need to, or modify existing ones 
-    - The isGameWon() functions has some bugs, you need to find the bugs and fix them 
-    - Implement the playing logic in the playGame function 
-    - Thinking of edge cases and handling them is a plus 
-    - executing 'await readInput();' in the playGame() function will read input from the console 
-*/
-
-
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
